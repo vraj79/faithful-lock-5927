5d3f7a36533f36205c923814291614ef82774875
shitalkumar87 <shitalsmn@gmail.com>
2023-01-19 09:05:25 +0530

    day2

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,19 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import {ChakraProvider} from "@chakra-ui/react"
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <ChakraProvider> 
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/router/AllRoutes.jsx
+++ frontend/src/router/AllRoutes.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Login from '../pages/Login/Login'
+import Signup from '../pages/Signup/Signup'
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/'  element={<p>Homepage</p>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path='/signup' element={<Signup/>}></Route>
     </Routes>
   )
 }
